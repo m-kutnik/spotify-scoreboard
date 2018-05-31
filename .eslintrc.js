83a2894787ad33f46e9e2c55087845cc1982a559
Mike <gimp2312@gmail.com>
2018-05-31 17:17:37 +0200

    add Vue plugin to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'prettier/prettier': 'error'
   },
+  plugins: ['vue'],
   parserOptions: {
     parser: 'babel-eslint'
   }
