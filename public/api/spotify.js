a1b701f6ea9ac70cd45b17c970c5042320886665
Mike <gimp2312@gmail.com>
2018-05-31 17:10:30 +0200

    create Spotify API

---
+++ public/api/spotify.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+class SpotifyApiInstance {
+  constructor() {
+    this.spotify = axios.create({
+      baseURL: 'https://api.spotify.com/',
+    })
+  }
+
+  checkToken(token) {
+    return this.spotify.get('/v1/me', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  }
+
+  setToken(token) {
+    this.spotify.defaults.headers.common.Authorization = `Bearer ${token}`
+  }
+
+  me() {
+    return this.spotify.get('/v1/me')
+  }
+
+  getPlaylists() {
+    return this.spotify.get('/v1/me/playlists?limit=50')
+  }
+
+  getTrackList(user, playlist, count) {
+    const promiseArray = [];
+    [...Array(count).keys()].forEach((i) => {
+      const promise = this.spotify.get(`/v1/users/${user}/playlists/${playlist}/tracks?offset=${i * 100}&limit=100`)
+      promiseArray.push(promise)
+    })
+    return promiseArray
+  }
+
+  getUserInfo(id) {
+    return this.spotify.get(`/v1/users/${id}`)
+  }
+}
+
+export default new SpotifyApiInstance()
