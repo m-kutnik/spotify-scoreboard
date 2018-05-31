10296a4a5d1654cbf887217a6e29a2ce8df78901
Mike <gimp2312@gmail.com>
2018-05-31 17:07:01 +0200

    fix linter errors

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 export default {
-  redirectURI: process.env.NODE_ENV === 'production' ? 'https://m-kutnik.github.io/spotify-scoreboard/redirect' : 'http://localhost:8080/redirect',
-  clientID: '9a1f73aa868942c2b70279ced0bae555',
-  scope: 'user-read-private user-read-email playlist-read-collaborative',
+  redirectURI:
+    process.env.NODE_ENV === "production"
+      ? "https://m-kutnik.github.io/spotify-scoreboard/redirect"
+      : "http://localhost:8080/redirect",
+  clientID: "9a1f73aa868942c2b70279ced0bae555",
+  scope: "user-read-private user-read-email playlist-read-collaborative",
 }
