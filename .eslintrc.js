a2a95ada26cae932276b7440ab2f80cc41af6efb
Mike <gimp2312@gmail.com>
2018-05-31 17:16:23 +0200

    change Vue Style Guide to Recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,13 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/prettier'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 'error'
   },
   parserOptions: {
     parser: 'babel-eslint'
