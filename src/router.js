b86fe2eeff3321f842c73b5ede63ea4bc5e1e108
Mike <gimp2312@gmail.com>
2018-06-02 01:48:51 +0200

    modify router.js

--- src/router.js
+++ src/router.js
@@ -1,21 +1,48 @@
 import Vue from "vue"
 import Router from "vue-router"
-import Home from "./views/Home.vue"
-import About from "./views/About.vue"
+import Spotify from "@/api/spotify"
+import Home from "@/views/Home"
+import Dashboard from "@/views/Dashboard"
+import Redirect from "@/views/Redirect"
 
 Vue.use(Router)
 
+const authUser = function authUser(next) {
+  const accessToken = window.localStorage.getItem("access_token")
+
+  if (accessToken === "" || accessToken === null) next("/")
+  Spotify.checkToken(accessToken)
+    .then(response => {
+      if (response.status !== 200) next("/")
+    })
+    .catch(err => {
+      if (err) next("/")
+    })
+
+  Spotify.setToken(accessToken)
+  next()
+}
+
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
-      name: "home",
+      name: "Home",
       component: Home,
     },
     {
-      path: "/about",
-      name: "about",
-      component: About,
+      path: "/dashboard",
+      name: "Dashboard",
+      component: Dashboard,
+      beforeEnter: (to, from, next) => {
+        authUser(next)
+      },
+    },
+    {
+      path: "/redirect",
+      name: "Redirect",
+      component: Redirect,
     },
   ],
 })
