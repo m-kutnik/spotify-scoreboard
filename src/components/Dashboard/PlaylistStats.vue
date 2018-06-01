69b17bfb05f4cbff51ea39bd703e234ef2c1861a
Mike <gimp2312@gmail.com>
2018-06-02 01:49:43 +0200

    create PlaylistStats component

---
+++ src/components/Dashboard/PlaylistStats.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="isActive">
+    <h2>Wow, there are <u>{{ count }}</u> tracks on this playlist</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    init: {
+      type: Number,
+      default() {
+        return 0
+      },
+    },
+    loader: {
+      type: Boolean,
+      default() {
+        return false
+      },
+    },
+  },
+  computed: {
+    isActive() {
+      return this.loader
+    },
+    count() {
+      return this.init
+    },
+  },
+  created() {
+    setTimeout(() => {
+      this.$emit("loaded", "playlistStats")
+    }, 500)
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  margin-bottom: 50px;
+  font-size: 2rem;
+}
+</style>
