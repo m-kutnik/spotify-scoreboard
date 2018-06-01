3010376d558a7a3bb95fd22f82c2c18b6c2a4e44
Mike <gimp2312@gmail.com>
2018-06-01 19:56:02 +0200

    create config.js

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  clientID: "9a1f73aa868942c2b70279ced0bae555",
+  // redirectURI: 'http://tardis.local:8080/redirect',
+  redirectURI: "http://localhost:8080/redirect",
+  scope: "user-read-private user-read-email playlist-read-collaborative",
+}
