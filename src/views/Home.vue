ab8280c58e26b87f7a0d8a53cfba2b7ebb1f770c
Mike <gimp2312@gmail.com>
2018-06-01 19:41:18 +0200

    modify Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="baner container">
+    <h1>Spotify Scoreboard</h1>
+    <h2>Scoreboard for your Spotify Collaborative playlists</h2>
+    <button
+      class="btn btn-login"
+      @click="login">
+      Login with Spotify</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue"
+import config from "@/config"
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld,
+  name: "Home",
+  data() {
+    return {}
+  },
+  methods: {
+    login() {
+      const clientID = config.clientID
+      const redirectURI = config.redirectURI
+      const scope = config.scope
+      const state = this.generateHash()
+
+      const url =
+        `https://accounts.spotify.com/authorize?response_type=token` +
+        `&client_id=${encodeURIComponent(clientID)}` +
+        `&scope=${encodeURIComponent(scope)}` +
+        `&redirect_uri=${encodeURIComponent(redirectURI)}` +
+        `&state=${encodeURIComponent(state)}`
+
+      window.location = url
+    },
+    generateHash() {
+      let text = ""
+      const possible =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+      for (let i = 0; i < 16; i += 1) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+      }
+
+      return text
+    },
   },
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 80%;
+  position: absolute;
+  text-align: center;
+  padding: 50px;
+  box-sizing: border-box;
+}
+
+h1 {
+  text-transform: uppercase;
+  font-size: 3rem;
+}
+h2 {
+  font-size: 2rem;
+}
+
+.btn-login {
+  margin-top: 100px;
+  font-size: 24px;
+}
+</style>
