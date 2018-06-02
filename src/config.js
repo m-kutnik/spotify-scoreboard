3dc7a9527d82d75b0ff5129aa8b9714c01a177a8
Mike <gimp2312@gmail.com>
2018-06-02 04:06:58 +0200

    fix config for Spotify API

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 export default {
   clientID: "9a1f73aa868942c2b70279ced0bae555",
   // redirectURI: 'http://tardis.local:8080/redirect',
-  redirectURI: "http://localhost:8080/redirect",
+  redirectURI:
+    process.env.NODE_ENV === "production"
+      ? "https://m-kutnik.github.io/spotify-scoreboard/#redirect"
+      : "http://localhost:8080/#redirect",
   scope: "user-read-private user-read-email playlist-read-collaborative",
 }
